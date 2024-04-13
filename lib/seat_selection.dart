bd9c4a88444552dd0d88da1ce18dbec005584352
kuklinski01 <155673167+kuklinski01@users.noreply.github.com>
2024-04-14 00:17:29 +0300

    Update seat_selection.dart

--- lib/seat_selection.dart
+++ lib/seat_selection.dart
@@ -23,119 +23,129 @@ class _SeatSelectionState extends State<SeatSelection> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Seats
-          Container(
-            width: 400,
-            height: 300,
-            padding: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: GridView.builder(
-              padding: EdgeInsets.zero,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 8,
-                childAspectRatio: 1,
-                crossAxisSpacing: 8,
-                mainAxisSpacing: 8,
-              ),
-              itemCount: 56,
-              itemBuilder: (BuildContext context, int index) {
-                int seatNumber = (index % 8) + 1; // Calculate seat number from 1 to 8
-
-                return GestureDetector(
-                  onTap: () => _toggleSeat(index),
-                  child: Container(
+    return SingleChildScrollView(
+      child: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Category Box
+            Container(
+              margin: EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // N/A Seat
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
                     decoration: BoxDecoration(
-                      color: _selectedSeats[index] ? Color(0xFF01163e) : Color(0xFF444451),
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(10),
-                        topRight: Radius.circular(10),
-                      ),
+                      color: const Color.fromARGB(255, 138, 129, 128),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    margin: EdgeInsets.all(5),
-                    width: 35,
-                    height: 30,
-                    child: Center(
-                      child: Text(
-                        '$seatNumber', // Display seat number from 1 to 8
-                        style: TextStyle(color: Colors.white),
-                      ),
+                    child: Text(
+                      'N/A',
+                      style: TextStyle(color: Colors.white),
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 20), // Add some spacing between the seats and the screen
-          // Theater Screen
-          Container(
-            width: 400,
-            height: 200,
-            decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Center(
-              child: Text(
-                'Theater Screen',
-                style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                  // Occupied Seat
+                  Container(
+                    margin: EdgeInsets.only(right: 10),
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 148, 17, 17),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      'Occupied',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  // Selected Seat
+                  Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 18, 173, 83),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Text(
+                      'Selected',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ),
-          SizedBox(height: 20), // Add some spacing between the screen and personal details
-          // Personal Details Box
-          Container(
-            width: 400,
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 42, 93, 170),
-              borderRadius: BorderRadius.circular(20),
+            // Seats
+            Container(
+              width: 400,
+              height: 300,
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: GridView.builder(
+                padding: EdgeInsets.zero,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 8,
+                  childAspectRatio: 1,
+                  crossAxisSpacing: 8,
+                  mainAxisSpacing: 8,
+                ),
+                itemCount: 56,
+                itemBuilder: (BuildContext context, int index) {
+                  int seatNumber = (index % 8) + 1; // Calculate seat number from 1 to 8
+
+                  return GestureDetector(
+                    onTap: () => _toggleSeat(index),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: _selectedSeats[index] ? Color.fromARGB(255, 51, 163, 75) : Color(0xFF444451),
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        ),
+                      ),
+                      margin: EdgeInsets.all(5),
+                      width: 35,
+                      height: 30,
+                      child: Center(
+                        child: Text(
+                          '$seatNumber', // Display seat number from 1 to 8
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Personal Details',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            SizedBox(height: 20), // Add some spacing between the seats and the screen
+            // Theater Screen with Image
+            Container(
+              width: 650,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  'https://dl.dropbox.com/scl/fi/6m7mf1v85ljjyh6c6wcoe/screen-edited.png?rlkey=lal8fo77d5oo28ksb6c4gvlyv&dl=0',
+                  width: 200,
+                  height: 900,
+                  fit: BoxFit.fill,
                 ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () {
-                    // Add your logic here to handle personal details submission
-                  },
-                  child: Text('Submit'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+    
+    
+    // Add some spacing at the end of the page
+ 
+                      
+          ],
+        ),
       ),
     );
   }
