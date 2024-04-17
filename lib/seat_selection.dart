0acad086a9c4e476ed4991106c5e96dadc41c99d
kuklinski01 <155673167+kuklinski01@users.noreply.github.com>
2024-04-17 13:03:02 +0300

    Update seat_selection.dart

--- lib/seat_selection.dart
+++ lib/seat_selection.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 void main() => runApp(MyApp());
 
@@ -31,17 +30,23 @@ class SeatSelection extends StatefulWidget {
 }
 
 class _SeatSelectionState extends State<SeatSelection> {
-  List<bool> _selectedSeats = List.generate(84, (index) => false);
+  List<bool> _selectedSeats = List.generate(98, (index) => false);
   int _selectedSeatCount = 0;
-  int _hoveredSeatIndex = -1;
+
   final double seatPrice = 12.50;
+  String _seatNumber = '';
 
   void _toggleSeat(int index) {
     setState(() {
       _selectedSeats[index] = !_selectedSeats[index];
       _selectedSeatCount += _selectedSeats[index] ? 1 : -1;
-      widget.onSeatSelected(_selectedSeatCount);
     });
+  }
+
+  void _displaySeatNumber(int index) {
+    int row = (index / 12).floor() + 1;
+    int column = index % 12 + 1;
+    _seatNumber = 'Seat: $row-$column';
   }
 
   double calculateTotalAmount() {
@@ -63,181 +68,159 @@ class _SeatSelectionState extends State<SeatSelection> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: SingleChildScrollView( // Added SingleChildScrollView
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Category Box
-              Container(
-                margin: EdgeInsets.only(bottom: 30),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // N/A Seat
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 138, 129, 128),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        'N/A',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    // Occupied Seat
-                    Container(
-                      margin: EdgeInsets.only(right: 10),
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 148, 17, 17),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        'Occupied',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                    // Selected Seat
-                    Container(
-                      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                      decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 18, 173, 83),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        'Selected',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              // Seats
-              Container(
-                width: 500,
-                height: 500,
-                padding: EdgeInsets.all(10),
-                child: GridView.builder(
-                  shrinkWrap: true,
-                  physics: NeverScrollableScrollPhysics(),
-                  padding: EdgeInsets.zero,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 12, // Updated to 13 seats per row
-                    childAspectRatio: 0.9,
-                    crossAxisSpacing: 7,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemCount: 84,
-                  itemBuilder: (BuildContext context, int index) {
-                    int row = index ~/ 12 + 1;
-                    int column = index % 12 + 1;
-                    return MouseRegion(
-                      onEnter: (_) {
-                        setState(() {
-                          // Change the color when the mouse enters
-                          _hoveredSeatIndex = index;
-                        });
-                      },
-                      onExit: (_) {
-                        setState(() {
-                          // Reset the color when the mouse exits
-                          _hoveredSeatIndex = -1;
-                        });
-                      },
-                      child: GestureDetector(
-                        onTap: () => _toggleSeat(index),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: _selectedSeats[index]
-                                ? Color.fromARGB(255,51, 163, 75)
-                                : (_hoveredSeatIndex == index ? Colors.grey : Color(0xFF444451)),
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(10),
-                              topRight: Radius.circular(10),
-                            ),
-                          ),
-                          margin: EdgeInsets.all(1),
-                          width: 20,
-                          height: 50,
-                          child: Center(
-                            child: Text(
-                              '$column',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 16,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-              SizedBox(height: 10), // Add some spacing between the seats and the screen
-              // Theater Screen with Image
-              Container(
-                width: 650,
-                height: 80,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(0),
-                ),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(20),
-                  child: Image.network(
-                    'https://dl.dropbox.com/scl/fi/6m7mf1v85ljjyh6c6wcoe/screen-edited.png?rlkey=lal8fo77d5oo28ksb6c4gvlyv&dl=0',
-                    width: 200,
-                    height: 900,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-              ),
-              SizedBox(height: 10), // Add some spacing between the theater screen and the selected seat count
-              // Selected Seat Count and Total Amount
-              Column(
-                children: [
-                  Align(
-                    alignment: Alignment.center,
+        body: Stack(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: SingleChildScrollView(
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text(
-                          'Selected Seats: $_selectedSeatCount',
-                          style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(height: 12),
-                        Padding(
-                          padding: EdgeInsets.only(top: 30),
-                          child: Container(
-                            padding: EdgeInsets.all(8),
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(255, 35, 6, 71), // Set the color of the container
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: Text(
-                              'Total Amount: ${calculateTotalAmount().toStringAsFixed(2)} €',
-                              style: TextStyle(fontSize: 13, fontWeight: FontWeight.bold, color: Color.fromARGB(255, 255, 252, 252)),
-                            ),
+                        Container(
+                          margin: EdgeInsets.only(bottom: 2, top: 20),
+                          padding: EdgeInsets.all(10),
+                          width: 300,
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 36, 44, 122),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              _infoBox('N/A', Color.fromARGB(255, 138, 129, 128)),
+                              _infoBox('Occupied', Color.fromARGB(255, 148, 17, 17)),
+                              _infoBox('Selected', Color.fromARGB(255, 22, 138, 138)),
+                            ],
                           ),
                         ),
-                        SizedBox(height: 10), // Add some space between the total amount and the button
-                        ElevatedButton(
-                          onPressed: () {
-                            // Add logic here to handle booking tickets
-                            print('Booking $_selectedSeatCount tickets'); // For demonstration, printing the number of selected seats
-                          },
-                          child: Text('Book Tickets'),
-                          
+                        Container(
+                          width: 700,
+                          height: 550,
+                          padding: EdgeInsets.all(20),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: GridView.builder(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  padding: EdgeInsets.zero,
+                                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 12,
+                                    childAspectRatio: 1.1,
+                                    crossAxisSpacing: 6,
+                                    mainAxisSpacing: 10,
+                                  ),
+                                  itemCount: 96,
+                                  itemBuilder: (BuildContext context, int index) {
+                                    int seatNumber = 12 - (index % 12);
+                                    return GestureDetector(
+                                      onTap: () => _toggleSeat(index),
+                                      child: AnimatedContainer(
+                                        duration: Duration(milliseconds: 200),
+                                        curve: Curves.ease,
+                                        decoration: BoxDecoration(
+                                          color: _selectedSeats[index] ? Color.fromARGB(255, 8, 23, 73) : Color.fromARGB(255, 121, 143, 156),
+                                          border: Border.all(color: Color.fromARGB(255, 255, 255, 255), width: 2),
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(15),
+                                            topRight: Radius.circular(15),
+                                          ),
+                                        ),
+                                        margin: EdgeInsets.all(5),
+                                        width: 10,
+                                        height: 30,
+                                        child: Center(
+                                          child: Text(
+                                            _selectedSeats[index] ? '${seatNumber}' : '',
+                                            style: TextStyle(
+                                              color: _selectedSeats[index] ? Colors.white : Color.fromARGB(255, 121, 143, 156),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 12,
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          _seatNumber,
+                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
                         ),
                       ],
                     ),
                   ),
-                ],
+                ),
+              ],
+            ),
+            Positioned(
+              top: 560,
+              left: 555,
+              child: Container(
+                width: 800,
+                height: 80,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(
+                    'https://dl.dropbox.com/scl/fi/6m7mf1v85ljjyh6c6wcoe/screen-edited.png?rlkey=lal8fo77d5oo28ksb6c4gvlyv&dl=0',
+                    fit: BoxFit.fill,
+                  ),
+                ),
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              bottom: 120,
+              right: 120,
+              child: Container(
+                
+                  
+                
+                padding: EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Selected Seats: $_selectedSeatCount',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Total Amount: ${calculateTotalAmount().toStringAsFixed(2)} €',
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => print('Booking $_selectedSeatCount tickets'),
+                      child: Text('Book Tickets'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+
+  Widget _infoBox(String label, Color color) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Text(
+        label,
+        style: TextStyle(color: Colors.white),
       ),
     );
   }
